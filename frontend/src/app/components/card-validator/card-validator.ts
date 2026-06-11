8c6274aeea19d2c741a926f0964f59087b9be990
UnderWater <131457895+FabianQuezada@users.noreply.github.com>
2026-06-11 11:23:35 -0400

    feat: add card-validator component
    Se creo el componente card-validator

---
+++ frontend/src/app/components/card-validator/card-validator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-validator',
+  imports: [],
+  templateUrl: './card-validator.html',
+  styleUrl: './card-validator.css',
+})
+export class CardValidator {}
